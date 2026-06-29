d59537011c316f4daa34bfc38ac6e1cfb5c18933
Tomlin7 <billydevbusiness@gmail.com>
2026-06-29 22:11:41 +0530

    www: migrate docs folder

--- www/app/docs/layout.tsx
+++ www/app/docs/layout.tsx
@@ -3,8 +3,13 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
+  const docsOptions = {
+    ...baseOptions(),
+    links: [], // Clear links in sidebar
+  };
+
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout tree={source.getPageTree()} {...docsOptions}>
       {children}
     </DocsLayout>
   );
